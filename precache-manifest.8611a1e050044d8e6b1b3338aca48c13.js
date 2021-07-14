6f7c76ca24ee9d97e24f2e809d2bc86295adcbad
gaki-1 <be10310.17@bitmesra.ac.in>
2021-07-14 17:30:12 +0530

    Updates

---
+++ precache-manifest.8611a1e050044d8e6b1b3338aca48c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733f2a506240555377c1933187292e9e",
+    "url": "/TodoAppList/index.html"
+  },
+  {
+    "revision": "eba0c518e4910408f3d2",
+    "url": "/TodoAppList/static/css/main.c58aa3a2.chunk.css"
+  },
+  {
+    "revision": "d5ea2f32eaa5703be429",
+    "url": "/TodoAppList/static/js/2.4d83187b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoAppList/static/js/2.4d83187b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba0c518e4910408f3d2",
+    "url": "/TodoAppList/static/js/main.d72232a9.chunk.js"
+  },
+  {
+    "revision": "0e24a7cc43a9f1376f71",
+    "url": "/TodoAppList/static/js/runtime-main.dbcced8d.js"
+  }
+]);
