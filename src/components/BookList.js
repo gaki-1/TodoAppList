e93064e12c5f672f1f58b80e7782ca667020f438
gaki-1 <be10310.17@bitmesra.ac.in>
2021-07-14 17:19:38 +0530

    todo list for hosting

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from '../components/BookDetails'
+
+const BookList = () => {
+    const { books } = useContext(BookContext)
+    return books.length ? (
+        <div className="book-list">
+            <ul>
+                {books.map(book => {
+                    return (<BookDetails book={book} key={book.id} />)
+                    })}
+            </ul>
+        </div>
+    ) : (<div className="empty">Congrats! You are done with all the tasks for the day</div >)
+}
+
+export default BookList;
